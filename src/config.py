9e30550ab7fdec02ec82d005382fd6b4f0a70e25
Brian Kim <bryo.kim1@gmail.com>
2024-04-12 14:20:12 +0300

    fix: add defaults for MONGODB_USER and MONGODB_PW

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ from urllib.parse import quote_plus
 class Config(BaseModel):
     # MongoDB settings
     MONGODB_URI: str = config("MONGODB_URI", "")
-    MONGODB_USER: str = quote_plus(config("MONGODB_USER"))
-    MONGODB_PW: str = quote_plus(config("MONGODB_PW"))
+    MONGODB_USER: str = quote_plus(config("MONGODB_USER", ""))
+    MONGODB_PW: str = quote_plus(config("MONGODB_PW", ""))
     MONGODB_HOST: str = config("MONGODB_HOST", "localhost")
     MONGODB_PORT: str = config("MONGODB_PORT", "27017")
     MONGODB_SCHEME: str = config("MONGODB_SCHEME", "mongodb")
