b7e04a7f2874af696e8215455f34c3f87e99d185
Brian Kim <bryo.kim1@gmail.com>
2024-04-12 14:15:35 +0300

    feat: add CORS

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database import init_db
 
@@ -25,4 +27,22 @@ app = FastAPI(
         "url": "https://github.com/Donut-Diaries/backend/blob/main/LICENSE",
     },
     lifespan=lifespan,
+)
+
+
+# Setup CORS
+
+LOCALHOST = os.getenv("DD_DEV_HOST", "")
+
+origins = ["https://dd-customer.vercel.app/"]
+
+if LOCALHOST:
+    origins.append(LOCALHOST)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
