f383e36316ecf676fe1934a00c3b29fc7e8eae73
Brian Kim <bryo.kim1@gmail.com>
2024-04-15 14:17:05 +0300

    refactor: inject consumer and credentials as dependencies

--- src/consumer/dependencies.py
+++ src/consumer/dependencies.py
@@ -14,22 +14,43 @@ from src.consumer.models import (
 )
 
 
-async def consumer_exists(
+def consumer_credentials(
     credentials: Annotated[SupabaseJWTPayload, Depends(SupabaseJWTBearer())]
-) -> Consumer | None:
-    """Validate that a consumer with given credentials exists
+) -> dict:
+    """Get dictionary representation of the SupabaseJWTPayload.
 
     Args:
-        credentials (SupabaseJWTPayload): Credentials of the
-            consumer to check.
+        credentials (SupabaseJWTPayload): Credentials of the vendor.
+
+    Returns:
+        dict: Dictionary of the values in SupabaseJWTPayload
+    """
+    return credentials.model_dump()
+
+
+async def get_consumer(
+    credentials: dict = Depends(consumer_credentials),
+) -> Consumer:
+    """Get a consumer from the database.
+
+    Args:
+        credentials (dict): Dictionary of the consumer's details from the
+            SupabaseJWTPayload.
+
+    Returns:
+        Consumer | None: A consumer from the database or None if not found.
+    """
+    return await Consumer.find_one({"_id": UUID(credentials["sub"])})
+
+
+async def consumer_exists(
+    consumer: Consumer = Depends(get_consumer),
+) -> Consumer | None:
+    """Validate that a consumer with given credentials exists
 
     Raises:
         HTTPException: If the consumer does not exist.
     """
-
-    consumer: Consumer | None = await Consumer.find_one(
-        {"_id": UUID(credentials.model_dump()["sub"])}
-    )
 
     if consumer:
         return consumer
@@ -38,34 +59,28 @@ async def consumer_exists(
 
 
 async def consumer_not_exists(
-    credentials: Annotated[SupabaseJWTPayload, Depends(SupabaseJWTBearer())]
+    consumer: Consumer = Depends(get_consumer),
 ) -> None:
     """Validate that a consumer with given credentials does not exist
-
-    Args:
-        credentials (SupabaseJWTPayload): Credentials of the
-            consumer to check.
 
     Raises:
         HTTPException: If the consumer already exists.
     """
-
-    consumer: Consumer = await Consumer.find_one(
-        {"_id": UUID(credentials.model_dump()["sub"])}
-    )
 
     if consumer:
         raise HTTPException(status_code=400, detail="Consumer already exists.")
 
 
 async def validate_anonymous_credentials(
-    credentials: Annotated[SupabaseJWTPayload, Depends(SupabaseJWTBearer())]
+    credentials: dict = Depends(consumer_credentials),
 ) -> AnonymousConsumerCreate | None:
     """
+    Gets credentials from the SupabaseJWT and uses them to create a 
+    AnonymousConsumerCreate object.
 
     Args:
-        credentials (SupabaseJWTPayload): Credentials of the user from the
-            Supabase JWT.
+        credentials (dict): Dictionary of the consumer's details from the
+            SupabaseJWTPayload.
 
     Raises:
         HTTPException: If there is a validation error
@@ -76,9 +91,8 @@ async def validate_anonymous_credentials(
     """
 
     try:
-        _credentials = credentials.model_dump()
         anonymous_consumer = AnonymousConsumerCreate(
-            id=_credentials["sub"], **_credentials
+            id=credentials["sub"], **credentials
         )
         return anonymous_consumer
     except ValidationError as e:
@@ -86,13 +100,15 @@ async def validate_anonymous_credentials(
 
 
 async def validate_signed_credentials(
-    credentials: Annotated[SupabaseJWTPayload, Depends(SupabaseJWTBearer())]
+    credentials: dict = Depends(consumer_credentials),
 ) -> SignedConsumerCreate | None:
     """
+    Gets credentials from the SupabaseJWT and uses them to create a 
+    SignedConsumerCreate object.
 
     Args:
-        credentials (SupabaseJWTPayload): Credentials of the user from the
-            Supabase JWT.
+        credentials (dict): Dictionary of the consumer's details from the
+            SupabaseJWTPayload.
 
     Raises:
         HTTPException: If there is a validation error
@@ -103,9 +119,8 @@ async def validate_signed_credentials(
     """
 
     try:
-        _credentials = credentials.model_dump()
         signed_consumer = SignedConsumerCreate(
-            id=_credentials["sub"], **_credentials
+            id=credentials["sub"], **credentials
         )
         return signed_consumer
     except ValidationError as e:
