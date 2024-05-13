deca48b7b5267603b9e30a1a4c81383b5c731773
Brian Kim <bryo.kim1@gmail.com>
2024-05-13 16:31:06 +0300

    feat: use AsyncMongoMockClient for tests by setting ENVIRONMENT=test

--- src/database.py
+++ src/database.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
+from mongomock_motor import AsyncMongoMockClient
 
 from src.config import CONFIG
+from src.logger import logger
 from src.consumer.models import Consumer, AnonymousConsumer, SignedConsumer
 
 from src.vendor.models import Vendor, Food
@@ -35,9 +37,22 @@ def get_mongodb_uri():
 
 async def init_db(app: FastAPI):
     """Initialize database"""
-    app.db = AsyncIOMotorClient(
-        get_mongodb_uri(), uuidRepresentation="standard"
-    ).donut_diaries
+
+    logger.info("Initializing database")
+
+    uri = get_mongodb_uri()
+
+    app.client = (
+        AsyncIOMotorClient(uri, uuidRepresentation="standard")
+        if CONFIG.ENVIRONMENT != "test"
+        else AsyncMongoMockClient(uri, uuidRepresentation="standard")
+    )
+
+    if isinstance(app.client, AsyncMongoMockClient):
+        logger.debug("Using test Mongo Client")
+
+    app.db = app.client.donut_diaries
+
     await init_beanie(
         app.db,
         document_models=[
